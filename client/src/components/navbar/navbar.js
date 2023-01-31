89d41faca8903fb711ed22b2c86a34c7c9d61ecf
CodeLee13 <leonpeterkin@icloud.com>
2023-01-31 11:38:48 +0000

    Testing

---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+
+    return (
+        <nav data-cy="navbar">
+            <p>This Day In History</p>
+            <button data-cy="login-button">Login</button>
+            <button data-cy="filter-button">Filter</button>
+
+        </nav>
+        
+    )
+}
+
+
+export default Navbar;
