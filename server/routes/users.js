778e808019028958fed02894881ac66e0bca254f
Matt Barber <mthbarber@gmail.com>
2023-02-01 12:26:58 +0000

    Add callback function that creates a new instance of User

---
+++ server/routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user')
+
+
+// Post request to create a new instance of User 
+router.post("/", async(req,res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.signup(email, password);
+        res.status(201).json({email})
+    }catch{
+        res.status(400).json({error: error.message})
+    }
+} );
+
+module.exports = router;
