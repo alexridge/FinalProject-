f92317f65a0dcb1812a85a65701ff44c1fb529fb
Matt Barber <mthbarber@gmail.com>
2023-02-02 11:02:11 +0000

    Create SignUpPage and test for rendering passes, created files for SignUpForm

---
+++ client/src/pages/signUpPage/SignUpPage.js
@@ -0,0 +1,18 @@
+import Navbar from "../../components/navbar/Navbar"
+
+
+const SignUpPage = () => {
+
+    return (
+        <body>
+            <div>
+                <Navbar />
+            </div>
+            <div>
+                <h2 data-cy="form-header">Sign Up</h2>
+            </div>
+        </body>
+    )
+}
+
+export default SignUpPage;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json())
 
+// Route set up 
 const usersRouter = require('./routes/users')
+
+//Connect to local MongoDB database
+mongoose.connect('mongodb://127.0.0.1:27017/TIH_test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=> {
+    console.log("Connection established")
+})
+.catch((err)=> {
+    console.log("Error occured");
+    console.log(err)
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
