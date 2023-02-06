cb1e7b05a31b180bf4edc526b7f5b093a84f6952
Matt Barber <mthbarber@gmail.com>
2023-02-06 17:14:33 +0000

    Alterations to ProfilePage for styling and rendering

--- client/src/pages/profilePage/ProfilePage.js
+++ client/src/pages/profilePage/ProfilePage.js
@@ -21,15 +21,15 @@ const ProfilePage = () => {
    
 
     return (
-        <div>
+        <div id="main-container">
             <Navbar />
             <h2>Welcome To Your Saved History</h2>
             <div>
                 {historyData != null && historyData.map((event) => {
                     return (
-                        <div key={event._id}>
-                            <p>{event.title}</p>
-                            <p>{event.text}</p>
+                        <div className='historical-event' key={event._id}>
+                            <h3>{event.title}</h3>
+                            <p className='event-text'>{event.text}</p>
                             <img src={event.image} alt="Historical" />
                         </div>
                     )
