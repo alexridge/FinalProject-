9be6b935cbf5138a5c0bb4e705f63a61531f2cab
Naomi Starr <naomi.starr.mail@gmail.com>
2023-02-07 14:53:14 +0000

    .envs added

--- client/src/components/homepageData.js
+++ client/src/components/homepageData.js
@@ -28,6 +28,9 @@ const HomepageData = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      const api_key = process.env.REACT_APP_API_KEY
+      const user_agent = process.env.REACT_APP_USER_AGENT;
+
       let today = new Date();
       let month = today.getMonth() + 1;
       let day = today.getDate();
@@ -39,8 +42,8 @@ const HomepageData = () => {
         method: "get",
         headers: {
           Authorization:
-            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJiMzZhYjNlOTY2MjU4MjA5YWY4OGEyZjdkYzFmZWZmNyIsImp0aSI6IjNjYmI0OGY1YjYwMThmODA2NDZlZDE0ZjBjNTMxMWM1ZTBlZjY5MGM0MzIwOTAxODIyMWMzZDgxOWVlZmZlZTY0ZTQzNDhkMWIwNTE5ODM2IiwiaWF0IjoxNjc1NzYzOTg2LjA1MDY2OSwibmJmIjoxNjc1NzYzOTg2LjA1MDY3MiwiZXhwIjoxNjc1Nzc4Mzg2LjA0NjMxOCwic3ViIjoiIiwiaXNzIjoiaHR0cHM6Ly9tZXRhLndpa2ltZWRpYS5vcmciLCJyYXRlbGltaXQiOnsicmVxdWVzdHNfcGVyX3VuaXQiOjUwMDAsInVuaXQiOiJIT1VSIn0sInNjb3BlcyI6WyJiYXNpYyJdfQ.MaT3cu67Jh9RujKRgkENvupcRgEcbXb_ulCZ9kPaxKn1pgn1qZXXBS7Jh7iU7a8qnePTd0j8Xb_rqG4oe9w_PD5tozdtaAwzcIpYo345Um90Q2nJo8S3yRYLd_mLGsG77OlzJCpg7BCP0cBgTGMWbEdKoBp787Rah-R7f5wRMXyHImqLGM1Jz8ke22QlQCkWa6yyuXt8V5KOvmQWwGVnT2OmRl2yPsskh9oJCsOiG6vWcIfFnP_LIrky11JGgBUM_ZY6yppY5Kr9hVQjls9wWv3o0oAKLzee0-HpSoOZdwTP_ddl4xd-qG7bjb-EjPcrhdWMelzNvEOQj5JXQL3DKWojWXHj17pTshMj6MeljSlxXZJrMdJbGmUXneDzuDOR3_8u0jGaKATpZfNSV3_Jdcl-dZUDRLL7w74Mx5XWIHEj_oI-53qCt5eb8HjGwr5yz36VGs-InlXZuPhUjA41AzZkAqxwY18priIiTX0CDcSJ61PKQTRVt7yluA4wTIn7BG_UIAEeUa692WUCnX_UhuqPRuRvuMl1Vx2HlyGFDYQMijHOrl3X_IFybpuyUp_EKRVy9N_31XVGP6G24ZvN7ZjpnFfpLhEdS4d8nxIJL7gvHjWYPm9uYSgbr6IUjr8DLGDoaAEKxcCmJZ3YSnVA7NHnzx2b-zh8J4i7_xJF2N8",
-          "Api-User-Agent": "This Day in History (alexridge2309@gmail.com)",
+            `Bearer ${api_key}`,
+          "Api-User-Agent": `${user_agent}`,
         },
       });
 
